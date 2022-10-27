74cb577c2d4096c8898b3f4bd2d760ad6ec5b66f
Victor Peixoto <victornovaisp@gmail.com>
2022-10-27 11:23:44 -0400

    Projeto Lista de Compras - JS OO

---
+++ assets/js/script.js
@@ -0,0 +1,136 @@
+class Produto {
+
+    constructor(){
+       this.id = 1;
+       this.arrayProdutos = [];
+       this.editId = null;
+       
+    }
+
+    lerDados(){
+        let produto = {};
+
+        produto.id = this.id;
+        produto.nomeProduto = document.getElementById('produto').value;
+        produto.preco = document.getElementById('preco').value;
+
+        return produto
+
+    }
+    
+    validaCampos(produto){
+        let msg = '';
+        
+        if(produto.nomeProduto == ''){
+            msg += 'Informe o nome do produto \n';
+        };
+        if(produto.preco == ''){
+            msg += 'Informe o preço do produto \n';
+        };
+
+        if(msg != ''){
+            alert(msg);
+            return false;
+        };
+
+        return true;
+
+    }
+
+    salvar(){
+        let produto = this.lerDados();
+        
+        if(this.validaCampos(produto)){
+            if(this.editId == null){
+                this.adicionar(produto);
+            } else {
+                this.atualizar(this.editId, produto);
+            };
+            
+        };
+        console.log(this.arrayProdutos);
+        this.listaTabela();
+        this.cancelar();
+
+    }
+
+    adicionar(produto){
+        produto.preco = parseFloat(produto.preco);
+        this.arrayProdutos.push(produto);
+        this.id++;
+    }
+
+    listaTabela(){
+        let tbody = document.getElementById('tbody');
+        tbody.innerText = '';
+        for(let i = 0; i < this.arrayProdutos.length; i++){
+            let tr = tbody.insertRow();
+
+            let td_id = tr.insertCell();
+            let td_produto = tr.insertCell();
+            let td_preco = tr.insertCell();
+            let td_acoes  = tr.insertCell();
+            
+            td_id.innerText = this.arrayProdutos[i].id;
+            td_produto.innerText = this.arrayProdutos[i].nomeProduto;
+            td_preco.innerText = this.arrayProdutos[i].preco;
+            td_id.classList.add('center');
+
+            let imgEdit = document.createElement('img');
+            let imgDelete = document.createElement('img')
+
+            imgEdit.src = './assets/img/edit.png';
+            imgEdit.setAttribute("onclick", "produto.preparaEdicao("+ JSON.stringify(this.arrayProdutos[i]) +")" );
+            
+            imgDelete.src = './assets/img/delete.png';            
+            imgDelete.setAttribute("onclick", "produto.deletar("+ this.arrayProdutos[i].id +")" );
+        
+            td_acoes.appendChild(imgEdit);
+            td_acoes.appendChild(imgDelete);
+            td_acoes.classList.add('center');
+        }
+    }
+
+    cancelar() {
+        document.getElementById('produto').value = '';
+        document.getElementById('preco').value = '';
+
+        document.getElementById('btn1').innerText = 'Salvar';
+        this.editId = null;
+
+    }
+
+    preparaEdicao(dados){
+        this.editId = dados.id;
+        document.getElementById('produto').value = dados.nomeProduto;
+        document.getElementById('produto').focus();
+        document.getElementById('preco').value = dados.preco;
+        document.getElementById('btn1').innerText = 'Atualizar';
+        
+    }
+
+    atualizar(id, produto){
+        for (let i = 0; i < this.arrayProdutos.length; i++) {
+            if(this.arrayProdutos[i].id == id){
+                this.arrayProdutos[i].nomeProduto = produto.nomeProduto;
+                this.arrayProdutos[i].preco = produto.preco;
+            }
+
+        }
+    }
+
+    deletar(id) {
+        if(confirm('Deseja realmente deletar o produto ID '+ id)){
+            let tbody = document.getElementById('tbody');
+            for(let i = 0; i < this.arrayProdutos.length; i++){
+                if(this.arrayProdutos[i].id == id){
+                    this.arrayProdutos.splice(i, 1);
+                    tbody.deleteRow(i)
+                }
+            }    
+        }    
+    }
+
+}
+
+var produto = new Produto();
